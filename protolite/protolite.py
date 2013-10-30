3b9d598098427e2e700f031a7108daf9ad5ebcf0
Andres Buritica <andres@thelinuxkid.com>
2013-10-30 03:40:53 +0000

    Use local variable when joining strings
    - optimization

--- protolite/protolite.py
+++ protolite/protolite.py
@@ -110,6 +110,9 @@ def _decode(proto, data):
     index = 0
     length = len(data)
     msg = dict()
+    # optmization: use local variable inside function
+    join = ''.join
+    # end optimization
     while index < length:
         value, index = decode_varint(data, index)
         field, wire = decode_key(value)
@@ -136,7 +139,7 @@ def _decode(proto, data):
             if _type == 'embedded':
                 msg[name] = _decode(info['message'], item)
             if _type == 'string':
-                msg[name] = ''.join([chr(i) for i in item])
+                msg[name] = join([chr(i) for i in item])
             if _type == 'repeated':
                 if name not in msg:
                     msg[name] = []
