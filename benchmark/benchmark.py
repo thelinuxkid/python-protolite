70832115b676306a97b9e48aa6df4c70effb516c
Andres Buritica <andres@thelinuxkid.com>
2013-11-02 01:45:52 +0000

    Add benchmarking tool

---
+++ benchmark/benchmark.py
@@ -0,0 +1,139 @@
+import timeit
+import logging
+import json
+
+log = logging.getLogger(__name__)
+log.name = 'protolite benchmark'
+
+
+protobuf_setup = """
+import messages_pb2
+"""
+protobuf = """
+user = messages_pb2.User(
+    userID=1007843487950966784L,
+    firstName='Foo',
+    lastName='Bar',
+    userType=messages_pb2.User.STANDARD,
+    homeAddress=messages_pb2.USAddress(
+        street='1 Main st',
+        city='Springfield',
+        state='MA',
+        zipCode='12345-6789',
+        country='USA',
+    ),
+    phoneNumbers=[
+        messages_pb2.PhoneNumber(
+            countryCode=1,
+            number='222-222-2222',
+        ),
+        messages_pb2.PhoneNumber(
+            countryCode=3,
+            number='444-444-4444',
+        ),
+    ],
+)
+
+data = user.SerializeToString()
+user = messages_pb2.User()
+user.ParseFromString(data)
+"""
+
+protolite_setup = """
+import messages
+"""
+protolite = """
+user = {
+    'user_id': 1007843487950966784L,
+    'first_name': 'Foo',
+    'last_name': 'Bar',
+    'user_type': messages.user.STANDARD,
+    'home_address': {
+        'street': '1 Main st',
+        'city': 'Springfield',
+        'state': 'MA',
+        'zip_code': '12345-6789',
+        'country': 'USA',
+    },
+    'phone_numbers': [
+        {
+            'country_code': 1,
+            'number': '222-222-2222',
+        },
+        {
+            'country_code': 3,
+            'number': '444-444-4444',
+        },
+    ],
+}
+data = messages.user.encode(user)
+messages.user.decode(data)
+"""
+
+
+def benchmark():
+    log.info('Calculating number of loops...')
+    # In order to make the overhead insignificant run the benchmark at
+    # least 100*overhead times.
+    overhead = timeit.repeat(stmt='pass')
+    target = min(overhead) * 100
+    # Find the number of loops by trying successive powers of 10 until the total
+    # time is >= overhead.
+    loops = 0
+    sample = 0
+    while sample < target:
+        loops += 1
+        # Use the slowest of the two code samples, protobuf
+        sample = timeit.timeit(
+            stmt=protobuf,
+            setup=protobuf_setup,
+            number=10**loops,
+        )
+    loops = 10**loops
+    repeat = 3
+
+    log.info('Running protobuf benchmark...')
+    times = timeit.repeat(
+        stmt=protobuf,
+        setup=protobuf_setup,
+        repeat=repeat,
+        number=loops,
+    )
+    buftime = min(times)
+    log.info('Running protolite benchmark...')
+    times = timeit.repeat(
+        stmt=protolite,
+        setup=protolite_setup,
+        repeat=repeat,
+        number=loops,
+    )
+    litetime = min(times)
+
+    bufmsg = dict([
+        ('loops', loops),
+        ('repeat', repeat),
+        ('secs', buftime),
+        ('speed', litetime/buftime),
+    ])
+    litemsg = dict([
+        ('loops', loops),
+        ('repeat', repeat),
+        ('secs', litetime),
+        ('speed', buftime/litetime),
+    ])
+    results = dict([
+        ('protobuf', bufmsg),
+        ('protolite', litemsg),
+    ])
+    log.info('Results:')
+    print json.dumps(results, indent=2)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s.%(msecs)03d %(name)s: %(levelname)s: %(message)s',
+        datefmt='%Y-%m-%dT%H:%M:%S',
+    )
+    log.info('Starting benchmark...')
+    benchmark()
---
+++ benchmark/messages.py
@@ -0,0 +1,184 @@
+import sys
+import json
+
+from collections import OrderedDict
+
+import protolite
+
+
+class decoding(object):
+    phone_number = {
+        1: {
+                "name": "country_code",
+                "type": "uint32"
+        },
+        2: {
+                "name": "number",
+                "type": "string"
+        }
+}
+    us_address = {
+        1: {
+                "name": "street",
+                "type": "string"
+        },
+        2: {
+                "name": "city",
+                "type": "string"
+        },
+        3: {
+                "name": "state",
+                "type": "string"
+        },
+        4: {
+                "name": "zip_code",
+                "type": "string"
+        },
+        5: {
+                "name": "country",
+                "type": "string"
+        }
+}
+    user = {
+        1: {
+                "name": "user_id",
+                "type": "uint64"
+        },
+        2: {
+                "name": "first_name",
+                "type": "string"
+        },
+        3: {
+                "name": "last_name",
+                "type": "string"
+        },
+        4: {
+                "name": "user_type",
+                "type": "enum"
+        },
+        5: {
+                "name": "home_address",
+                "type": "embedded"
+        },
+        6: {
+                "name": "phone_numbers",
+                "type": "repeated"
+        }
+}
+
+
+class encoding(object):
+    phone_number = {
+        "country_code": {
+                "type": "uint32",
+                "field": 1
+        },
+        "number": {
+                "type": "string",
+                "field": 2
+        }
+}
+    us_address = {
+        "street": {
+                "type": "string",
+                "field": 1
+        },
+        "city": {
+                "type": "string",
+                "field": 2
+        },
+        "state": {
+                "type": "string",
+                "field": 3
+        },
+        "zip_code": {
+                "type": "string",
+                "field": 4
+        },
+        "country": {
+                "type": "string",
+                "field": 5
+        }
+}
+    user = {
+        "user_id": {
+                "type": "uint64",
+                "field": 1
+        },
+        "first_name": {
+                "type": "string",
+                "field": 2
+        },
+        "last_name": {
+                "type": "string",
+                "field": 3
+        },
+        "user_type": {
+                "type": "enum",
+                "field": 4
+        },
+        "home_address": {
+                "type": "embedded",
+                "field": 5
+        },
+        "phone_numbers": {
+                "type": "repeated",
+                "field": 6
+        }
+}
+
+
+
+class convenience_coder(object):
+
+    def __init__(self, decoding, encoding):
+        self.decoding = decoding
+        self.encoding = encoding
+
+
+    def decode(self, message):
+        return protolite.decode(self.decoding, message)
+
+
+    def encode(self, message):
+        return protolite.encode(self.encoding, message)
+
+
+    def _pprint(self, message, encoding):
+        fields = OrderedDict()
+        for k,v in message.items():
+            if encoding[k]['type'] == 'enum':
+                v = encoding[k]['message'][v]
+            if encoding[k]['type'] in ['embedded', 'repeated']:
+                v = self._pprint(v, encoding[k]['message'])
+            fields[k] = v
+        return fields
+
+
+    def pprint(self, message, encoding=None, stream=sys.stdout):
+        if encoding is None:
+            encoding = self.encoding
+        fields = self._pprint(message, encoding)
+        stream.write(json.dumps(fields, indent=8, separators=(',', ': ')))
+
+
+phone_number = convenience_coder(decoding.phone_number, encoding.phone_number)
+us_address = convenience_coder(decoding.us_address, encoding.us_address)
+user = convenience_coder(decoding.user, encoding.user)
+
+
+user.STANDARD = 0
+user.ADMIN = 1
+
+
+user.user_type = dict()
+user.user_type[0] = "STANDARD"
+user.user_type[1] = "ADMIN"
+
+decoding.user[5]["message"] = decoding.us_address
+decoding.user[6]["message"] = decoding.phone_number
+
+encoding.user["home_address"]["message"] = encoding.us_address
+encoding.user["phone_numbers"]["message"] = encoding.phone_number
+
+encoding.user["user_type"]["message"] = user.user_type
